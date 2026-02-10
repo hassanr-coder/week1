fc0decaf75bc77da81ad2c0e64731b811358218c
hassanr-coder <hassanr@students.trinitydc.edu>
2026-02-10 14:23:57 -0500

    Assignment2

--- Assignment1.py
+++ Assignment1.py
@@ -4,8 +4,5 @@ CMSC 111
 Spring 2026
 Assignment 1 Week 1
 '''
-age = int(input("Enter your age: ")) 
-print("Hello, World!")
-print(f"I am {age} years old.")
-print("You are", age, "years old.")
-
+age=int(input("Enter your age:"))
+print("Your age is:", age)
